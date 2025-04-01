950f74a3981fa510621b4b3bd017a409c476c174
kylie <ksjturner00@gmail.com>
2025-04-01 13:27:04 +0200

    change server.js and root package-lock.json

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,9 @@ const seriesRoutes = require('./routes/series.routes.js')
 const authRoutes = require('./routes/auth.routes.js')
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
     });
 }
 
