cacf83d4c926789c9f18ad9f671d12fca22a1936
kylie <ksjturner00@gmail.com>
2025-04-07 10:51:23 +0200

    change server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,6 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cookieParser());
 
 // CORS Configuration
 const corsOrigin = process.env.NODE_ENV === "production" 
