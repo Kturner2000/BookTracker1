4af623918a7163e62b7cd587724b886a8ba242b8
kylie <ksjturner00@gmail.com>
2025-04-13 13:46:15 +0200

    update server

--- backend/server.js
+++ backend/server.js
@@ -45,14 +45,7 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
    
-    connectDB()
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error("Database connection error:", err);
-        process.exit(1); // Exit the process if the database connection fails
-    });
+    console.log(`server is running on port ${PORT}`);
+    
+    connectDB();
 });
